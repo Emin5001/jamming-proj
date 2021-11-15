e0cc087af85eeeacc3edcb049165291e63bb8c2c
Emin Koroglu <eminnkoroglu.1@gmail.com>
2021-11-15 13:11:43 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+        {/*Add SearchBar Component */}
+          <div className="App-playlist">
+        {/*Add SearchResults Component */}
+        {/*Add Playlist Component */}
+          </div>
+        </div>
+      </div>
+    );  
+  }
+
 }
 
 export default App;
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './SearchResults.css'
+
+export class SearchResults extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* Add a tracklist component */}
+          </div>
+        )
+    }
+}
